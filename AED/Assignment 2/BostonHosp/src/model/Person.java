9cf0d661cf120f6abe3ffa4036dd2c76eb536488
Tanvivalkunde <valkunde.t@northeastern.edu>
2022-10-27 17:05:46 -0400

    gender

---
+++ AED/Assignment 2/BostonHosp/src/model/Person.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Tanvi
+ */
+public class Person {
+      private int id;
+    private String ssn;
+    private String name;
+    private Date dateOfBirth;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private Gender gender;
+    private Role role;
+    private House residence;
+
+    public Person(String ssn, String name, Date dateOfBirth, String email, String password, String phoneNumber, Gender gender, Role role, House residence) {
+        this.ssn = ssn;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.gender = gender;
+        this.role = role;
+        this.residence = residence;
+        
+    
+    
+}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public House getResidence() {
+        return residence;
+    }
+
+    public void setResidence(House residence) {
+        this.residence = residence;
+    }
+}
